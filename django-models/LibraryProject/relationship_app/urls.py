84eb10541aa7786f547f04e15b05890595a6c0ad
Cwills <ampocoll.wills@gmail.com>
2025-09-13 22:21:18 -0800

    Update all files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register  # custom view for registration
+from . import views   # import the module, so we can do views.register
 
 urlpatterns = [
-    # existing URLs…
+    # your other URLs
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),  # <-- this line has views.register
 ]
