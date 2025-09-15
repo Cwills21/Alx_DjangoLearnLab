12f10c5be433826cf3562255e379042c427dc94d
Cwills <ampocoll.wills@gmail.com>
2025-09-14 16:00:03 -0800

    Add all Filles

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,13 +1,18 @@
+# from django.contrib import admin
+# from .models import Book
+
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     # Columns to display in the list view
+#     list_display = ('title', 'author', 'publication_year')
+
+#     # Filters on the right side
+#     list_filter = ('publication_year', 'author')
+
+#     # Search bar fields
+#     search_fields = ('title', 'author')
+# bookshelf/admin.py
 from django.contrib import admin
-from .models import Book
+from .models import CustomUser  # register your CustomUser instead of Book
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    # Columns to display in the list view
-    list_display = ('title', 'author', 'publication_year')
-
-    # Filters on the right side
-    list_filter = ('publication_year', 'author')
-
-    # Search bar fields
-    search_fields = ('title', 'author')
+admin.site.register(CustomUser)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,44 @@
-# LibraryProject/bookshelf/models.py
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+# Custom manager
+class CustomUserManager(BaseUserManager):
+    """
+    Manager for CustomUser
+    """
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        """
+        Create and return a regular user.
+        """
+        if not username:
+            raise ValueError('The Username field is required')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        """
+        Create and return a superuser.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
+
+# Custom user model
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()  # Attach our manager here
 
     def __str__(self):
         return self.username
